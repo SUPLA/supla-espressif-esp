aa351cdedf46e621e705e3bc26ee12f34d6a7b08
Przemek Zygmunt <przemek@supla.org>
2017-12-18 12:30:27 +0100

    Version increment

--- src/user/supla_esp.h
+++ src/user/supla_esp.h
@@ -25,7 +25,7 @@
 #include "board/supla_esp_board.h"
 #include "espmissingincludes.h"
 
-#define SUPLA_ESP_SOFTVER "2.5.3"
+#define SUPLA_ESP_SOFTVER "2.5.5"
 
 #define LO_VALUE  0
 #define HI_VALUE  1
