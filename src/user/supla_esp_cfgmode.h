b877d3232718dde08abc82051d79fa29c98c551a
Przemek Zygmunt <przemek@supla.org>
2018-09-16 14:49:37 +0200

    Duplicate declaration removed

--- src/user/supla_esp_cfgmode.h
+++ src/user/supla_esp_cfgmode.h
@@ -19,7 +19,6 @@
 #ifndef SUPLA_ESP_CFGMODE_H_
 #define SUPLA_ESP_CFGMODE_H_
 
-char ICACHE_FLASH_ATTR supla_esp_cfgmode_started(void);
 void ICACHE_FLASH_ATTR supla_esp_cfgmode_start(void);
 char ICACHE_FLASH_ATTR supla_esp_cfgmode_started(void);
 void ICACHE_FLASH_ATTR supla_esp_channel_value_changed(int channel_number, char v);
