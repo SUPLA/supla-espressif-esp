8f4ecf33d3f2ef04120586bf2e815d608a713659
Przemek Zygmunt <przemek@supla.org>
2018-10-29 12:22:40 +0100

    supla-common updated to the latest development version

--- supla-common/proto.h
+++ supla-common/proto.h
@@ -323,6 +323,7 @@ extern "C" {
 #define SUPLA_MFR_NICE 5
 #define SUPLA_MFR_ITEAD 6
 #define SUPLA_MFR_VL 7
+#define SUPLA_MFR_HPOL 8
 
 #pragma pack(push, 1)
 
@@ -513,6 +514,7 @@ typedef struct {
 
   _supla_int_t Flags;
   _supla_int16_t ManufacturerID;
+  _supla_int16_t ProductID;
 
   unsigned char channel_count;
   TDS_SuplaDeviceChannel_C
@@ -659,6 +661,7 @@ typedef struct {
   _supla_int_t AltIcon;
   _supla_int_t UserIcon[USER_ICON_MAXCOUNT];
   _supla_int16_t ManufacturerID;
+  _supla_int16_t ProductID;
 
   unsigned _supla_int_t Flags;
   unsigned char ProtocolVersion;
@@ -920,6 +923,7 @@ typedef struct {
   _supla_int_t price_per_unit;  // * 0.0001
   // Currency Code A https://www.nationsonline.org/oneworld/currencies.htm
   char currency[3];
+  char custom_unit[5];  // including the terminating null byte ('\0')
 
   unsigned _supla_int64_t counter;
   _supla_int64_t calculated_value;   // * 0.001
@@ -940,6 +944,7 @@ typedef struct {
   _supla_int_t Result;
 } TSC_SuperUserAuthorizationResult;  // v. >= 10
 
+// CALCFG == CALIBRATION / CONSIG
 typedef struct {
   _supla_int_t ChannelID;
   _supla_int_t Command;
