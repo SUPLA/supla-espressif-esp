eae44861d4b027f036fa01737884a6a554cca285
Przemysław Zygmunt <p.zygmunt@acsoftware.pl>
2018-12-28 16:16:12 +0100

    Version increment

--- src/user/supla_esp.h
+++ src/user/supla_esp.h
@@ -25,7 +25,7 @@
 #include "board/supla_esp_board.h"
 #include "espmissingincludes.h"
 
-#define SUPLA_ESP_SOFTVER "2.7.2"
+#define SUPLA_ESP_SOFTVER "2.7.3"
 
 #ifndef ESP8266_SUPLA_PROTO_VERSION
 #define ESP8266_SUPLA_PROTO_VERSION SUPLA_PROTO_VERSION
