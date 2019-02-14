d5b29438305d16b4bb943d40c4d4877ad00e746c
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2019-02-14 12:38:10 +0100

    docker files moved to the https://github.com/SUPLA/supla-docker/tree/espressif repository

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker build -t "devel/esp8266" .
--- docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PROJ=~/CProjects
-[ -e $PROJ ] || mkdir -p $PROJ
-docker run -v "$PROJ":/CProjects -it devel/esp8266 /bin/bash
