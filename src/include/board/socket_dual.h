e030e1236fac6b3a99ced325b7577b40c9ca119f
kris <kris.sg@wp.pl>
2019-05-27 17:37:58 +0200

    socket_dual pliki "c" i "h"

---
+++ src/include/board/socket_dual.h
@@ -0,0 +1,61 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef SOCKET_DUAL_H_
+#define SOCKET_DUAL_H_
+
+#define ESP8266_SUPLA_PROTO_VERSION 7
+
+#define SUPLA_ESP_SOFTVER "2.7.4.0"
+
+//#define _RASING_EDGE
+
+#define USE_GPIO9
+#define USE_GPIO14
+
+#ifdef __BOARD_socket_dual_ds18b20
+	#define DS18B20
+	#define TEMPERATURE_CHANNEL 2
+	#define AP_SSID "SOCKET_DUAL_DS18B20"
+
+    	#define W1_GPIO14
+
+#endif
+
+#ifdef __BOARD_socket_dual_DHT22
+	#define DHTSENSOR
+   	#define TEMPERATURE_HUMIDITY_CHANNEL 2
+	#define AP_SSID "SOCKET_DUAL_DHT22"
+
+        #define W1_GPIO14
+
+#endif
+
+#ifdef __BOARD_socket_dual
+	#define AP_SSID "SOCKET_DUAL"
+#endif
+
+#define LED_RED_PORT      2
+#define B_RELAY1_PORT     4
+#define B_RELAY2_PORT    12
+#define B_CFG_PORT        0
+
+
+void supla_esp_board_send_channel_values_with_delay(void *srpc);
+
+#endif
