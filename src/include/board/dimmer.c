731a5ee4cc0a566f84654f2142e3fbe5401b047c
kris <kris.sg@wp.pl>
2019-06-11 16:38:54 +0200

    usuniecie hi - blad

--- src/include/board/dimmer.c
+++ src/include/board/dimmer.c
@@ -133,12 +133,10 @@ char ICACHE_FLASH_ATTR supla_esp_board_set_rgbw_value(int ChannelNumber, int *Co
 
 	dimmer_brightness = *Brightness;
 	
-	hi = supla_esp_gpio_output_is_hi(B_RELAY1_PORT);
-	
 	if ( dimmer_brightness > 100 )
 		dimmer_brightness = 100;
 	
-	if ( hi = 1 )
+	if ( supla_esp_gpio_output_is_hi(B_RELAY1_PORT) = 1 )
 		supla_esp_pwm_set_percent_duty(50, 100, 0);
 	
 	supla_esp_pwm_set_percent_duty(dimmer_brightness, 100, 0);
