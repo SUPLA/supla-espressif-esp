cb599b9ebf44b6be0a9d38c1c4eb783a88d4b210
kris <kris.sg@wp.pl>
2019-06-13 17:52:35 +0200

    pwm test 2

--- src/user/supla_esp_gpio.c
+++ src/user/supla_esp_gpio.c
@@ -1269,12 +1269,13 @@ supla_esp_gpio_set_hi(int port, char hi) {
 	BOARD_GPIO_OUTPUT_SET_HI
 	#endif
 	
-	if ( hi == 1 )  {
-		supla_log(LOG_DEBUG, "warunek hi = %i", hi); 
 	if ( port == 4 )  {
-		supla_log(LOG_DEBUG, "warunek port = %i", port);
+		supla_log(LOG_DEBUG, "warunek port = %i", port); 
+	if ( hi == 1 )  {
+		supla_log(LOG_DEBUG, "warunek hi = %i", hi);
 		supla_esp_pwm_set_percent_duty(50, 100, 0);
-	}
+	} else
+	{ supla_esp_pwm_set_percent_duty(0, 100, 0); }
 	}
 
 	if ( port == 16 ) {
