e6aa637c02f135d229e7b1059319565d1ee30940
Espablo <espablo@o2.pl>
2019-07-16 10:33:48 +0200

    add board inCan

--- src/build.sh
+++ src/build.sh
@@ -210,6 +210,30 @@ case $1 in
      FOTA=1
      EXTRA_CCFLAGS="-DSRPC_QUEUE_SIZE=4 -DSRPC_QUEUE_MIN_ALLOC_COUNT=4 -DESP8266_LOG_DISABLED=1"
    ;;
+   "inCan_DS")  
+	SPI_MODE="QIO"
+    FLASH_SIZE="4096"       
+   ;;
+   "inCan_DHT11")      
+	SPI_MODE="QIO"
+    FLASH_SIZE="4096"       
+   ;;
+   "inCan_DHT22")      
+	SPI_MODE="QIO"
+    FLASH_SIZE="4096"       
+   ;;
+   "inCanRS_DS") 
+    SPI_MODE="QIO"
+    FLASH_SIZE="4096"     
+   ;;
+   "inCanRS_DHT11")  
+ 	SPI_MODE="QIO"
+    FLASH_SIZE="4096"       
+   ;;
+   "inCanRS_DHT22") 
+	SPI_MODE="QIO"
+    FLASH_SIZE="4096"       
+  ;;
 	
    *)
    echo "Usage:"
@@ -244,6 +268,12 @@ case $1 in
    echo "              lightswitch_x2_DHT22";
    echo "              lightswitch_x2_54_DHT22";
    echo "              impulse_counter";
+   echo "              inCan_DS";
+   echo "              inCan_DHT11";
+   echo "              inCan_DHT22";
+   echo "              inCanRS_DS";
+   echo "              inCanRS_DHT11";
+   echo "              inCanRS_DHT22";
    echo 
    echo
    exit;
