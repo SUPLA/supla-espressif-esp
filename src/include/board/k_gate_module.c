79c5595aefc7b64afe7d62b1e1c93cd700faf884
kris <kris.sg@wp.pl>
2019-10-10 14:41:09 +0200

    wylaczenie ustawienia domyslnego dla kanalu sensor

--- src/include/board/k_gate_module.c
+++ src/include/board/k_gate_module.c
@@ -73,7 +73,7 @@ void ICACHE_FLASH_ATTR supla_esp_board_set_channels(TDS_SuplaDeviceChannel_B *ch
 	channels[1].Number = 1;
 	channels[1].Type = SUPLA_CHANNELTYPE_SENSORNO;
 	channels[1].FuncList = 0;
-	channels[1].Default = SUPLA_CHANNELFNC_SENSORNO;
+	channels[1].Default = 0;
 	channels[1].value[0] = 0;
 
 }
