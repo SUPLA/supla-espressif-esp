f4011639c02bd1adcd623bc42e20c775eed27c12
kris <kris.sg@wp.pl>
2019-11-26 16:56:02 +0100

    dzwigniecie wersji dopasowanie kodu do nowej wersji

--- src/include/board/k_socket.h
+++ src/include/board/k_socket.h
@@ -21,7 +21,7 @@
 
 #define ESP8266_SUPLA_PROTO_VERSION 7
 
-#define SUPLA_ESP_SOFTVER "2.7.12.1"
+#define SUPLA_ESP_SOFTVER "2.7.16.0"
 
 #define BOARD_CFG_HTML_TEMPLATE
 #define BOARD_ON_CONNECT
@@ -56,7 +56,9 @@
  	supla_log(LOG_DEBUG, "update, port = %i", port); \
 	supla_esp_cfg.FirmwareUpdate = 1;\
 	supla_esp_cfg_save(&supla_esp_cfg);\
-	supla_esp_devconn_system_restart(); };  }; 
+	os_delay_us(200); \
+	supla_system_restart(); };\
+};
 
 
 char *ICACHE_FLASH_ATTR supla_esp_board_cfg_html_template(
