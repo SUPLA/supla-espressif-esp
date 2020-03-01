8850ea27cdeca836a5c6e62aac4f90013f246586
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-03-01 15:55:50 +0100

    time_ms param added

--- src/user/supla_esp_devconn.c
+++ src/user/supla_esp_devconn.c
@@ -352,12 +352,15 @@ supla_esp_devconn_send_channel_values_cb(void *ptr) {
 }
 
 void DEVCONN_ICACHE_FLASH
-supla_esp_devconn_send_channel_values_with_delay(void) {
-
+supla_esp_devconn_send_channel_values_with_delay(int time_ms) {
 	os_timer_disarm(&devconn->supla_value_timer);
 	os_timer_setfn(&devconn->supla_value_timer, (os_timer_func_t *)supla_esp_devconn_send_channel_values_cb, NULL);
-	os_timer_arm(&devconn->supla_value_timer, 1500, 0);
+	os_timer_arm(&devconn->supla_value_timer, time_ms, 0);
+}
 
+void DEVCONN_ICACHE_FLASH
+supla_esp_devconn_send_channel_values_with_delay(void) {
+	supla_esp_devconn_send_channel_values_with_delay(1500);
 }
 
 void DEVCONN_ICACHE_FLASH
