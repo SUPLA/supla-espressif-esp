908921357fc54f5d11be8e7294b21fa175af944d
kris <kris.sg@wp.pl>
2020-04-10 19:11:24 +0200

    dodanie zmiennej rs

--- src/include/board/k_rs_module.c
+++ src/include/board/k_rs_module.c
@@ -299,12 +299,15 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_on_input_active(void* _input_cfg)
     supla_input_cfg_t* input_cfg = (supla_input_cfg_t*)_input_cfg; 
     if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE_RS) { 
     supla_log(LOG_DEBUG, "RELAY HI"); 
-
+	int rs;
+	rs = 0;
 #ifdef _ROLLERSHUTTER_SUPPORT 
 	supla_roller_shutter_cfg_t *rs_cfg = supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id); 
 	if ( rs_cfg != NULL ) { 
         if ( 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->up) || 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->down)) { 
+			rs = rs+1;
 			supla_esp_gpio_rs_set_relay(rs_cfg, RS_RELAY_OFF, 1, 1);
+			supla_log(LOG_DEBUG, "rs = %i", rs);
 			supla_log(LOG_DEBUG, "RS_RELAY_OFF");
           }			 
 	else { 
