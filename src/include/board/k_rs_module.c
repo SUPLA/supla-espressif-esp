7458c4a258c2425bd23a7b7c7031e6b79f525768
kris <kris.sg@wp.pl>
2020-04-10 13:22:22 +0200

    usuniecie zbednego warunku

--- src/include/board/k_rs_module.c
+++ src/include/board/k_rs_module.c
@@ -303,13 +303,11 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_on_input_active(void* _input_cfg)
 
     supla_log(LOG_DEBUG, "RELAY HI");
  #ifdef _ROLLERSHUTTER_SUPPORT
-	if(supla_esp_cfg.RollerShutter == ROLLER_SHUTTER_ON) {
 			supla_roller_shutter_cfg_t *rs_cfg = supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id);
 			if ( rs_cfg != NULL ) {
 				// supla_esp_gpio_rs_set_relay(rs_cfg, RS_RELAY_OFF, 1, 1);
 				// supla_esp_gpio_rs_set_relay(rs_cfg, RS_RELAY_OFF, 0, 0);
 			}
-	}
 #endif /*_ROLLERSHUTTER_SUPPORT*/
 
     } else if (input_cfg->type == INPUT_TYPE_BTN_BISTABLE || input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE) {
@@ -335,7 +333,6 @@ supla_esp_board_gpio_on_input_inactive(void* _input_cfg)
 
    supla_log(LOG_DEBUG, "RELAY LO");
    #ifdef _ROLLERSHUTTER_SUPPORT
-		if(supla_esp_cfg.RollerShutter == ROLLER_SHUTTER_ON) {
 			supla_roller_shutter_cfg_t *rs_cfg = supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id);
 			if ( rs_cfg != NULL ) {
 
@@ -351,7 +348,6 @@ supla_esp_board_gpio_on_input_inactive(void* _input_cfg)
 					supla_esp_gpio_rs_set_relay(rs_cfg, rs_cfg->up->gpio_id == input_cfg->relay_gpio_id ? RS_RELAY_UP : RS_RELAY_DOWN, 1, 1);
 				}
 			}
-		}
    #endif /*_ROLLERSHUTTER_SUPPORT*/ 
  
     } else if (input_cfg->type == INPUT_TYPE_BTN_BISTABLE) {
