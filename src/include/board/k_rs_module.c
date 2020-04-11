f5431c10037d6bddbffacbf6ddf1f69b8a089696
kris <kris.sg@wp.pl>
2020-04-11 07:30:15 +0200

    button fix

--- src/include/board/k_rs_module.c
+++ src/include/board/k_rs_module.c
@@ -305,10 +305,10 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_on_input_active(void* _input_cfg)
 #ifdef _ROLLERSHUTTER_SUPPORT 
 	supla_roller_shutter_cfg_t *rs_cfg = supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id); 
 	if ( rs_cfg != NULL ) { 
+		rs = rs+1;
         if ( 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->up) || 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->down)) { 
-			rs = rs+1;
-			supla_log(LOG_DEBUG, "rs = %i", rs);
-			if ( rs == 0 ) {
+			supla_log(LOG_DEBUG, "if gpio hi, rs = %i", rs);
+			if ( rs == 1 ) {
 				supla_esp_gpio_rs_set_relay(rs_cfg, RS_RELAY_OFF, 1, 1);
 				supla_log(LOG_DEBUG, "RS_RELAY_OFF");
 			}
