a5affaeaa979c82b1ba981e3b692bfc30603b6c6
kris <kris.sg@wp.pl>
2020-04-15 18:28:37 +0200

    dht22 fix 2

--- src/user/user_main.c
+++ src/user/user_main.c
@@ -146,10 +146,12 @@ void MAIN_ICACHE_FLASH user_init(void)
 	
 		if ( supla_esp_cfg.ThermometerType == 1 ) {
 			supla_ds18b20_init();
+			supla_log(LOG_DEBUG, "DS18B20 INIT" );
 		}
 		
 		if ( supla_esp_cfg.ThermometerType == 2 ) {
 			supla_dht_init();
+			supla_log(LOG_DEBUG, "DHT22 INIT" );
 		}
 	#else
 		
@@ -185,13 +187,27 @@ void MAIN_ICACHE_FLASH user_init(void)
     	 return;
      }
 
+	#if defined TEMP_SELECT
+	
+		if ( supla_esp_cfg.ThermometerType == 1 ) {
+			supla_ds18b20_start();
+			supla_log(LOG_DEBUG, "DS18B20 START" );
+		}
+		
+		if ( supla_esp_cfg.ThermometerType == 2 ) {
+			supla_dht_start();
+			supla_log(LOG_DEBUG, "DHT22 START" );
+		}
+	#else
 
-    #ifdef DS18B20
+     #ifdef DS18B20
 		supla_ds18b20_start();
-    #endif
+     #endif
 
-	#ifdef DHTSENSOR
+	 #ifdef DHTSENSOR
 		supla_dht_start();
+	 #endif
+	 
 	#endif
 
 	#ifdef ELECTRICITY_METER_COUNT
