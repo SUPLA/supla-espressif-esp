ecbdac89a5eb2dd15743564c07b7b10c8390834c
kris <kris.sg@wp.pl>
2020-04-23 15:56:57 +0200

    wlaczenie wew pullup, poprawki kodu

--- src/include/board/k_rs_module_v2.c
+++ src/include/board/k_rs_module_v2.c
@@ -92,7 +92,7 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_init(void) {
 	//PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
 	//PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14);
 	//PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO0_U);	// pullup gpio 0
-	//PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO4_U);	// pullup gpio 4
+	PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO4_U);	// pullup gpio 4
 	PIN_PULLUP_EN(PERIPHS_IO_MUX_MTDI_U);		// pullup gpio 12	
 	PIN_PULLUP_EN(PERIPHS_IO_MUX_MTMS_U);		// pullup gpio 14
 	
@@ -325,11 +325,13 @@ void ICACHE_FLASH_ATTR supla_esp_board_gpio_on_input_active(void* _input_cfg) {
     if (input_cfg->type == INPUT_TYPE_BTN_MONOSTABLE_RS) {
 		
     supla_log(LOG_DEBUG, "RELAY HI RS");
-   // #ifdef _ROLLERSHUTTER_SUPPORT
+	
+  #ifdef _ROLLERSHUTTER_SUPPORT
 		supla_roller_shutter_cfg_t *rs_cfg = supla_esp_gpio_get_rs__cfg(input_cfg->relay_gpio_id);
 		
 		supla_log(LOG_DEBUG, "active rs btn up = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->up));
 		supla_log(LOG_DEBUG, "active rs btn down = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->down));
+		
 		/*if ( rs_cfg != NULL ) {
 
 			if ( 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->up) || 1 == __supla_esp_gpio_relay_is_hi(rs_cfg->down)) {
