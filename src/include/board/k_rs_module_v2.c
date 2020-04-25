dbaacf384300da07f4807e6e22ca45a189e0fe69
kris <kris.sg@wp.pl>
2020-04-25 09:24:31 +0200

    poprawienie warunku

--- src/include/board/k_rs_module_v2.c
+++ src/include/board/k_rs_module_v2.c
@@ -365,13 +365,14 @@ supla_esp_board_gpio_on_input_inactive(void* _input_cfg) {
 				supla_log(LOG_DEBUG, "if 1 rs btn up = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->up)); 
 				supla_log(LOG_DEBUG, "if 1 rs btn down = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->down));
 			}
-		}		  
+				  
 		else {
 
-		supla_esp_gpio_rs_set_relay(rs_cfg, rs_cfg->up->gpio_id == input_cfg->relay_gpio_id ? RS_RELAY_UP : RS_RELAY_DOWN, 1, 1);
+			supla_esp_gpio_rs_set_relay(rs_cfg, rs_cfg->up->gpio_id == input_cfg->relay_gpio_id ? RS_RELAY_UP : RS_RELAY_DOWN, 1, 1);
 		
-		supla_log(LOG_DEBUG, "set rs btn up = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->up)); 
-		supla_log(LOG_DEBUG, "set rs btn down = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->down));
+			supla_log(LOG_DEBUG, "set rs btn up = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->up)); 
+			supla_log(LOG_DEBUG, "set rs btn down = %i", __supla_esp_gpio_relay_is_hi(rs_cfg->down));
+			}
 		}
 
 	#endif /*_ROLLERSHUTTER_SUPPORT*/ 
