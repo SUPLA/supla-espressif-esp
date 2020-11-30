97c11a41cb68822664a7d74499fa67441ca8c7b8
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2020-11-30 21:01:46 +0100

    Verify DONT_SAVE_STATE macro

--- src/user/supla_esp_devconn.c
+++ src/user/supla_esp_devconn.c
@@ -998,6 +998,7 @@ supla_esp_channel_set_value(TSD_SuplaChannelNewValue *new_value) {
 		if ( ColorBrightness > 100 )
 			ColorBrightness = 0;
 
+        #ifndef DONT_SAVE_STATE
 		if (new_value->ChannelNumber < RS_MAX_COUNT) {
 			#ifdef RGBW_ONOFF_SUPPORT
 			if ( new_value->ChannelNumber == rgb_cn ) {
@@ -1022,6 +1023,7 @@ supla_esp_channel_set_value(TSD_SuplaChannelNewValue *new_value) {
 						supla_esp_state.turnedOff[new_value->ChannelNumber] |= 0x2;
 					}
 				}
+                #endif /*DONT_SAVE_STATE*/
 			} else if ( new_value->ChannelNumber == dimmer_cn
 					RGBW_CHANNEl_CMP ) {
 
@@ -1048,6 +1050,7 @@ supla_esp_channel_set_value(TSD_SuplaChannelNewValue *new_value) {
 			}
 			#endif /*RGBW_ONOFF_SUPPORT*/
 		}
+        #endif /*DONT_SAVE_STATE*/
 
 		#ifdef RGBW_ONOFF_SUPPORT
 		   supla_esp_channel_set_rgbw_value(new_value->ChannelNumber, Color, ColorBrightness, Brightness, TurnOnOff, 1, 1);
