a9ec2e194df7f49b36b43925a2d355b3e9930a19
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-01-11 23:19:21 +0100

    supla-common update

--- supla-common/lck.h
+++ supla-common/lck.h
@@ -19,16 +19,38 @@
 #ifndef LCK_H_
 #define LCK_H_
 
+#if defined(ESP8266) || defined(ESP32)
+#include <mem.h>
+#define LCK_ICACHE_FLASH ICACHE_FLASH_ATTR
+#endif
+
+#ifndef LCK_ICACHE_FLASH
+#define LCK_ICACHE_FLASH
+#endif /*LCK_ICACHE_FLASH*/
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void *lck_init(void);
-void lck_lock(void *lck);
-char lck_lock_with_timeout(void *lck, int timeout_sec);
-void lck_unlock(void *lck);
-int lck_unlock_r(void *lck, int result);
-void lck_free(void *lck);
+#ifdef __DEBUG
+// #define __LCK_DEBUG
+#endif
+
+#ifdef __LCK_DEBUG
+#define lck_lock(ptr) __lck_lock(ptr, __FILE__, __LINE__)
+void LCK_ICACHE_FLASH _lck_lock(void *lck);
+void LCK_ICACHE_FLASH __lck_lock(void *lck, const char *file, int line);
+void LCK_ICACHE_FLASH lck_debug_init(void);
+void LCK_ICACHE_FLASH lck_debug_dump(void);
+#else
+void LCK_ICACHE_FLASH lck_lock(void *lck);
+#endif /*__LCK_DEBUG*/
+
+char LCK_ICACHE_FLASH lck_lock_with_timeout(void *lck, int timeout_sec);
+void LCK_ICACHE_FLASH lck_unlock(void *lck);
+int LCK_ICACHE_FLASH lck_unlock_r(void *lck, int result);
+void *LCK_ICACHE_FLASH lck_init(void);
+void LCK_ICACHE_FLASH lck_free(void *lck);
 
 #ifdef __cplusplus
 }
