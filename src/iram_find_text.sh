a2f26b273f0c0dc287fd3b4a8244532b42478878
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-01-11 23:19:14 +0100

    iram_find_text script added

--- src/iram_find_text.sh
+++ src/iram_find_text.sh
@@ -1,1 +1,4 @@
 /hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av user/.output/eagle/debug/lib/libuser.a |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av supla-dev/.output/eagle/debug/lib/libsupladev.a |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av driver/.output/eagle/debug/lib/libdriver.a  |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av nettle/.output/eagle/debug/lib/libnettle.a  |grep .text.
