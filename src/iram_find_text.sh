ced1e688b9a0ac54547099fa5aca6675c480d921
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-01-12 14:40:55 +0100

    Patch change

--- src/iram_find_text.sh
+++ src/iram_find_text.sh
@@ -1,4 +1,4 @@
-/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av user/.output/eagle/debug/lib/libuser.a |grep .text.
-/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av supla-dev/.output/eagle/debug/lib/libsupladev.a |grep .text.
-/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av driver/.output/eagle/debug/lib/libdriver.a  |grep .text.
-/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/../lib/gcc/xtensa-lx106-elf/4.8.2/../../../../xtensa-lx106-elf/bin/nm -av nettle/.output/eagle/debug/lib/libnettle.a  |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/xtensa-lx106-elf-nm -av user/.output/eagle/debug/lib/libuser.a |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/xtensa-lx106-elf-nm -av supla-dev/.output/eagle/debug/lib/libsupladev.a |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/xtensa-lx106-elf-nm -av driver/.output/eagle/debug/lib/libdriver.a  |grep .text.
+/hdd2/Espressif/sdk_3x/xtensa-lx106-elf/bin/xtensa-lx106-elf-nm -av nettle/.output/eagle/debug/lib/libnettle.a  |grep .text.
