82bfcbc57886c27045568bbdbf1c7b8c92941d0a
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-07-28 20:20:35 +0200

    Missing slash

--- src/user/supla_esp_mqtt.c
+++ src/user/supla_esp_mqtt.c
@@ -1349,7 +1349,7 @@ uint8 ICACHE_FLASH_ATTR supla_esp_mqtt_ha_em__prepare_message(
   }
 
   const char cfg[] =
-      "{\"avty\":{\"topic\":\"%s/state"
+      "{\"avty\":{\"topic\":\"%s/state/"
       "connected\",\"payload_available\":\"true\",\"payload_not_available\":"
       "\"false\"},\"~\":\"%s/channels/"
       "%i\",\"device\":{\"ids\":\"%s\",\"mf\":\"%s\",\"name\":\"%s\",\"sw\":\"%"
@@ -1622,7 +1622,7 @@ uint8 ICACHE_FLASH_ATTR supla_esp_mqtt_ha_relay_prepare_message(
       "execute_action\",\"dev_cla\":\"shutter\",\"pl_open\":\"REVEAL\",\"pl_"
       "cls\":\"SHUT\",\"pl_stop\":\"STOP\",\"set_pos_t\":\"~/set/"
       "closing_percentage\",\"pos_t\":\"~/state/"
-      "shut\",\"pos_open\":0,\"pos_clsd\":100,\"avty_t\":\"%s/state"
+      "shut\",\"pos_open\":0,\"pos_clsd\":100,\"avty_t\":\"%s/state/"
       "connected\",\"pl_avail\":\"true\",\"pl_not_avail\":\"false\",\"pos_"
       "tpl\":\"{%% if value is defined %%}{%% if value | int < 0 %%}0{%% elif "
       "value | int > 100 %%}100{%% else %%}{{value | int}}{%% endif %%}{%% "
