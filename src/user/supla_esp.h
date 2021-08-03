6be8edbae74bfc0ef54fe3661cc9dadb4fb71192
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-08-03 20:02:10 +0200

    Version increment

--- src/user/supla_esp.h
+++ src/user/supla_esp.h
@@ -22,7 +22,7 @@
 #include "board/supla_esp_board.h"
 #include "espmissingincludes.h"
 
-#define SUPLA_ESP_SOFTVER "2.8.20"
+#define SUPLA_ESP_SOFTVER "2.8.21"
 
 #define STATE_UNKNOWN 0
 #define STATE_DISCONNECTED 1
