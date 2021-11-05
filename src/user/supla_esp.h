cc4e52e8ffa492273163aec02a713ae57101c6a6
Przemek Zygmunt <p.zygmunt@acsoftware.pl>
2021-11-05 16:00:30 +0100

    v2.8.41

--- src/user/supla_esp.h
+++ src/user/supla_esp.h
@@ -22,7 +22,7 @@
 #include "board/supla_esp_board.h"
 #include "espmissingincludes.h"
 
-#define SUPLA_ESP_SOFTVER "2.8.40"
+#define SUPLA_ESP_SOFTVER "2.8.41"
 
 #define STATE_UNKNOWN 0
 #define STATE_DISCONNECTED 1
